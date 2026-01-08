c4bfdddd57d2e79c1b39daecf9db3d845327388e
Amogh1511 <amoghmath2000@gmail.com>
2026-01-08 16:46:46 +0530

    RegressionModel

---
+++ regression_app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = tf.keras.models.load_model('regressionmodel.h5')
+
+# Load the encoders and scaler
+with open('GenderEncoder.pkl', 'rb') as file:
+    labelencoder = pickle.load(file)
+
+with open('GeographyEncoder.pkl', 'rb') as file:
+    onehotencode = pickle.load(file)
+
+with open('Scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+## streamlit app
+st.title('Customer Churn PRediction')
+
+# User input
+geography = st.selectbox('Geography', onehotencode.categories_[0])
+gender = st.selectbox('Gender', labelencoder.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+exited = st.selectbox('Exited',[0,1])
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+
+
+# Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [labelencoder.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'Exited': [exited]
+})
+
+# One-hot encode 'Geography'
+geo_encoded = onehotencode.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehotencode.get_feature_names_out(['Geography']))
+
+# Combine one-hot encoded columns with input data
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+# Scale the input data
+input_data_scaled = scaler.transform(input_data)
+
+
+
+# Predict churn
+prediction = model.predict(input_data_scaled)
+prediction_salary = prediction[0][0]
+
+st.write(f'Prediction Estimated Salary: ${prediction_salary:.2f}')
+
+if prediction_salary > 0.5:
+    st.write('The customer is likely to churn.')
+else:
+    st.write('The customer is not likely to churn.')
